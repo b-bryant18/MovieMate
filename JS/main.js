8e146c517e2563a5d3671220d46125b5b5c31c24
Brad Bryant <bradleybryant86@gmail.com>
2020-08-17 00:39:44 -0500

    Wrote jquery function to get searchText value and make axios call to api

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,22 @@
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
-        console.log($('#searchText').val());
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
         e.preventDefault();
     })
-}); 
+});
+
+// Looks at searchForm ID and upon submit takes the searchText val and passes it to 
+//the getMovies function created below.
+
+function getMovies(searchText) {
+    axios.get('http://www.omdbapi.com?s' + searchText)
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+}
+// .then will work if we get data back from axios.get
+//If we get an error back console.log it
