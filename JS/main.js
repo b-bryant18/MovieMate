4e1ec98e885c7f9669d43a743711d6ea7a6fb4b6
Brad Bryant <bradleybryant86@gmail.com>
2020-08-18 00:21:19 -0500

    Made movies and output vars with jQuery each method to display data object when submitted

--- JS/main.js
+++ JS/main.js
@@ -13,6 +13,20 @@ function getMovies(searchText) {
     axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=c969a4af&t=' + searchText)
         .then((response) => {
             console.log(response)
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie) => {
+                output += `
+                <div class="col-md-3">
+                <div class= "well text-center">
+                <img src ="movie.Poster">
+                <h5>${movie.Title}</h5>
+                <a onClick="movieSelected('${movie.imdbID}') class="btn btn-primary" href="#">Movie Details</a>
+                //imdbID needs to be in quotes or console will think it's a var
+                </div>
+                </div>
+                `
+            });
         })
         .catch((err) => {
             console.log(err)
