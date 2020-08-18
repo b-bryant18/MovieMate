6f190f8a7d429e2525dd4718c7da2f7a39f06294
Brad Bryant <bradleybryant86@gmail.com>
2020-08-18 17:31:11 -0500

    Copied code from index to movie.html but removed jumbotron

--- JS/main.js
+++ JS/main.js
@@ -11,28 +11,31 @@ $(document).ready(() => {
 
 function getMovies(searchText) {
     axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=c969a4af&t=' + searchText)
+        //Need to include api key, t= means title + user input
         .then((response) => {
-            console.log(response)
+            console.log(response);
             let movies = response.data.Search;
             let output = '';
             $.each(movies, (index, movie) => {
+                //movie is arbitrary name for data
                 output += `
                 <div class="col-md-3">
-                <div class= "well text-center">
-                <img src ="movie.Poster">
-                <h5>${movie.Title}</h5>
-                <a onClick="movieSelected('${movie.imdbID}') class="btn btn-primary" href="#">Movie Details</a>
-                //imdbID needs to be in quotes or console will think it's a var
+                    <div class= "well text-center">
+                        <img src ="${movie.Poster}">
+                             <h5>${movie.Title}</h5>
+                                <a onClick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#"> Movie Details </a>
+                        //imdbID needs to be in quotes or console will think it's a var
+                    </div>
                 </div>
-                </div>
-                `
+                `;
             });
+            console.log($('#movies'));
+            $('#movies').html(output);
         })
         .catch((err) => {
             console.log(err)
         });
 }
-// .then will work if we get data back from axios.get
-//Need to include api key, t= means title + user input
+
 //.catch will log errors to console
 
